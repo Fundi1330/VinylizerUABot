f36275bdf5dea92e76be880d9c69862ea1d4e29a
Fundi1330 <kordaaroma@gmail.com>
2025-04-15 13:13:03 +0300

    feat: change command messages

--- bot/commands/help.py
+++ bot/commands/help.py
@@ -2,4 +2,13 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Test message')
+    help_message = '''
+Я - бот-вінілізатор💽. Я можу перетворити вашу пісню на відео-кружечок у вигляді вінілової пластинки♻️.
+Бот дозволяє детально налаштувати кожен аспект відео🔧.
+Ось список команд:
+- /start - запустити бота.
+- /help - отримати це повідомлення.
+- /vinylize - розпочати процес вінілізації.
+'''
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=help_message)

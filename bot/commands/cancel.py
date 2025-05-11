f6ce9728dc474fa8443379003f8ddd7f27005ef7
Fundi1330 <kordaaroma@gmail.com>
2025-05-11 13:55:03 +0300

    misc: fixed cancel command message

--- bot/commands/cancel.py
+++ bot/commands/cancel.py
@@ -2,5 +2,5 @@ from telegram import Update
 from telegram.ext import ContextTypes, ConversationHandler
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message('Процес було успішно зупинено!')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Процес було успішно зупинено!')
     return ConversationHandler.END

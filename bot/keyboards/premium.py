51ec2567e9a97f2e1356521de2e4cc7eeaa94212
Fundi1330 <lerchmap@gmail.com>
2026-02-16 18:01:21 +0200

    fix: /premium keyboard duplicates each time you run the command

--- bot/keyboards/premium.py
+++ bot/keyboards/premium.py
@@ -2,13 +2,13 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Bot, LabeledPri
 from bot.config import config
 
 # the callback data is stored like this "duration of the subscription(in months), price"
-keyboard = []
 plans: dict = config.get('plans', None)
 
 if plans is None:
     raise ValueError('"plans" should be set in config')
 
 async def generate_premium_invoice_keyboard(bot: Bot):
+    keyboard = []
     for plan in plans:
         title = f"Купити преміум на {plan['length']} місяць"
         link = await bot.create_invoice_link(

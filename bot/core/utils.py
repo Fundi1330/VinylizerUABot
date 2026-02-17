f65d512bd65a2fa69ca56d61ed06fe979c6ffdb2
Fundi1330 <lerchmap@gmail.com>
2026-02-17 09:45:07 +0200

    fix(video rendering): utility functions now return str and not Path

--- bot/core/utils.py
+++ bot/core/utils.py
@@ -4,17 +4,17 @@ from pathlib import Path
 
 vinyl_map_path = Path(config.get('assets_path'), 'default', 'vinyl_map.json')
 
-def get_default_image() -> Path:
-        return Path(config.get('default_assets_path'), 'vinyl', 'vinyl_default_center.png')
+def get_default_image() -> str:
+        return str(Path(config.get('default_assets_path'), 'vinyl', 'vinyl_default_center.png'))
     
-def get_vinyl_noise() -> Path:
-    return Path(config.get('default_assets_path'), 'vinyl_noise.mp3')
+def get_vinyl_noise() -> str:
+    return str(Path(config.get('default_assets_path'), 'vinyl_noise.mp3'))
     
-def get_result_path(username: str, id: int) -> Path:
-    return Path(config.get('assets_path'), f'results/{username}_{id}/')
+def get_result_path(username: str, id: int) -> str:
+    return str(Path(config.get('assets_path'), f'results/{username}_{id}/'))
 
-def get_cover_path(username: str, id: int) -> Path:
-    return Path(config.get('assets_path'), f'covers/{username}_{id}/')
+def get_cover_path(username: str, id: int) -> str:
+    return str(Path(config.get('assets_path'), f'covers/{username}_{id}/'))
 
 def get_vinyl_list() -> list[dict]:
     with open(vinyl_map_path, 'r') as f:

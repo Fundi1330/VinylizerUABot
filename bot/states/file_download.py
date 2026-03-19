0402bfc0fb034b3570922c1a0eeda39b1c1e933c
Fundi1330 <lerchmap@gmail.com>
2026-03-19 22:51:29 +0200

    refactor(file downloading): make file downloading cleaner and separated from message sending

--- bot/states/file_download.py
+++ bot/states/file_download.py
@@ -21,71 +21,34 @@ CONFIGURE = 0
 
 executor = ThreadPoolExecutor(max_workers=4)
 
+class AllowedVideoDurationExceededError(Exception):
+    pass
+
 def run_save_audio_process(cmd_args: list, context: ContextTypes.DEFAULT_TYPE, save_path: str):
     subprocess.run(cmd_args, check=True)
     audio = AudioClip(save_path)
     context.user_data['audio_path'] = save_path
     context.user_data['audio_duration'] = audio.duration
 
-async def download_audio(audio: Audio, chat_id: int, context: ContextTypes.DEFAULT_TYPE, user: User, max_duration: int) -> int:
+async def download_audio(audio: Audio, context: ContextTypes.DEFAULT_TYPE, audio_path: str, cover_path: str, max_duration: int) -> None:
     if audio.duration > max_duration:
-        time_left = datetime.timedelta(seconds=max_duration)
-        text = f'''
-            ❌Довживна занадто велика. Ваш аккаунт дозволяє завантажувати максимальною довжиною в {
-                format_timedelta(time_left, format="short", locale="uk_UA")
-            }
-        '''
-        await context.bot.send_message(chat_id=chat_id, text=text)
-        return -1
+        raise AllowedVideoDurationExceededError()
     
     file_id = audio.file_id
-    try:
-        new_file = await context.bot.get_file(file_id)
-    except BadRequest as e:
-        text = '''
-            ❌Розмір аудіо занадто великий. Спробуйте зменшити його розмір
-        '''
-        await context.bot.send_message(chat_id=chat_id, text=text)
-        return -1
-    audio_folder = get_user_audio_path(user.username, user.id)
-
-    audio_name = f"{uuid.uuid4()}.mp3"
-    audio_path = str(Path(audio_folder) / audio_name)
-    await new_file.download_to_drive(audio_path)
+    file = await context.bot.get_file(file_id)
+    await file.download_to_drive(audio_path)
     context.user_data['audio_path'] = audio_path
-    cover_name = f'{uuid.uuid4()}.png'
-    cover_path = str(Path(get_cover_path(user.username, user.id), cover_name))
     cover = save_audio_cover(audio_path, cover_path)
     if cover:
         context.user_data['cover_path'] = cover
-    return 0
 
-async def download_video(video: Video, chat_id: int, context: ContextTypes.DEFAULT_TYPE, user: User, max_duration: int) -> int:
+async def download_video(video: Video, context: ContextTypes.DEFAULT_TYPE, audio_folder: str, max_duration: int) -> None:
     if video.duration > max_duration:
-        time_left = datetime.timedelta(seconds=max_duration)
-        text = f'''
-            ❌Довживна відео занадто велика. Ваш аккаунт дозволяє завантажувати відео максимальною довжиною в {
-                format_timedelta(time_left, format="short", locale="uk_UA")
-            }
-        '''
-        await context.bot.send_message(chat_id=chat_id, text=text)
-        return -1
-    text = '''
-        🔃Відео завантажується...
-    '''
-    message = await context.bot.send_message(chat_id=chat_id, text=text)
+        raise AllowedVideoDurationExceededError()
     file_id = video.file_id
-    try:
-        new_file = await context.bot.get_file(file_id)
-    except BadRequest as e:
-        text = '''
-            ❌Розмір відео занадто великий. Спробуйте зменшити його розмір, або ви можете завантажити його на ютуб
-        '''
-        await context.bot.send_message(chat_id=chat_id, text=text)
-        return -1
-    audio_folder = get_user_audio_path(user.username, user.id)
+    file = await context.bot.get_file(file_id)
 
-    video_path = await new_file.download_to_drive(str(Path(audio_folder) / video.file_name))
+    video_path = await file.download_to_drive(str(Path(audio_folder) / video.file_name))
     audio_name = f'{uuid.uuid4()}.mp3'
     save_path = str(Path(audio_folder) / audio_name)
     ffmpeg_cmd = [
@@ -103,21 +66,12 @@ async def download_video(video: Video, chat_id: int, context: ContextTypes.DEFAU
         await asyncio.get_running_loop().run_in_executor(
             executor, run_save_audio_process, ffmpeg_cmd, context, save_path
         )
-        text = '''
-            📩Відео успішно завантажено!
-        '''
-        await message.edit_text(text=text)
         os.remove(video_path)
     except subprocess.CalledProcessError as e:
         logger.error(f'An error occured while converting the video to audio: {e}')
-        text = '''
-            Під час завантаження відео виникла помилка!
-        '''
-        await context.bot.send_message(chat_id=chat_id, text=text)
-        return -1
-    return 0
+        raise
 
-async def download_audio_from_youtube(link: str, chat_id: int, context: ContextTypes.DEFAULT_TYPE, user: User, max_duration: int) -> int:
+async def download_audio_from_youtube(link: str, chat_id: int, context: ContextTypes.DEFAULT_TYPE, audio_folder: str, cover_folder: str, max_duration: int) -> None:
     # Check video duration before downloading
     def get_video_duration():
         ydl_opts = {
@@ -133,29 +87,14 @@ async def download_audio_from_youtube(link: str, chat_id: int, context: ContextT
         video_duration = await loop.run_in_executor(executor, get_video_duration)
         
         if video_duration > max_duration:
-            time_left = datetime.timedelta(seconds=max_duration)
-            text = f'''
-                ❌Відео занадто велике. Ваш акаунт дозволяє завантажувати відео максимальною довжиною в {
-                    format_timedelta(time_left, format="short", locale="uk_UA")
-                }
-            '''
-            await context.bot.send_message(chat_id=chat_id, text=text)
-            return -1
+            raise AllowedVideoDurationExceededError()
     except Exception as e:
         logger.error(f'An error occurred while checking video duration: {e}')
-        return -1
-    
-    text = '''
-        🔃Відео завантажується...
-    '''
-    message = await context.bot.send_message(chat_id=chat_id, text=text)
-    
-    audio_folder = get_user_audio_path(user.username, user.id)
-    cover_folder = get_cover_path(user.username, user.id)
+        raise e
     
     audio_id = str(uuid.uuid4())
-    audio_output_template = str(Path(audio_folder) / audio_id)
-    cover_output_template = str(Path(cover_folder) / audio_id)
+    audio_output_template = str(Path(audio_folder, audio_id))
+    cover_output_template = str(Path(cover_folder, audio_id))
     audio_path = f'{audio_output_template}.mp3'
     cover_path = f'{cover_output_template}.webp'
 
@@ -197,20 +136,12 @@ async def download_audio_from_youtube(link: str, chat_id: int, context: ContextT
         context.user_data['audio_path'] = audio_path
         context.user_data['audio_duration'] = audio.duration
         context.user_data['cover_path'] = cover_path
-        
-        edited_text = '''
-            📩Ютуб-відео успішно завантажено!
-        '''
-        await message.edit_text(text=edited_text)
     except Exception as e:
         logger.error(f'An error occurred while downloading from YouTube: {e}')
         edited_text = '''
             Під час завантаження відео виникла помилка!
         '''
         await context.bot.send_message(chat_id=chat_id, text=edited_text)
-        return -1
-    
-    return 0
     
 
 async def file_download_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -218,22 +149,68 @@ async def file_download_callback(update: Update, context: ContextTypes.DEFAULT_T
     user = get_or_create_user(update.effective_user.id)
 
     max_duration = get_max_duration(user.is_premium)
+    chat_id = update.effective_chat.id
 
-    result = None
-    if update.message.audio:
-        result = await download_audio(update.message.audio, update.effective_chat.id, context, update.effective_user, max_duration)
-    elif update.message.video and user.is_premium:
-        result = await download_video(update.message.video, update.effective_chat.id, context, update.effective_user, max_duration)
-    elif update.message.text and user.is_premium:
-        result = await download_audio_from_youtube(update.message.text, update.effective_chat.id, context, update.effective_user, max_duration)
-    else:
-        text = '''
-            Дана функція доступна лише преміум-користувачам.
-            \n/premium
+    audio_folder = get_user_audio_path(update.effective_user.username, update.effective_user.id)
+    cover_folder = get_cover_path(update.effective_user.username, update.effective_user.id)
+    audio_name = f"{uuid.uuid4()}.mp3"
+    audio_path = str(Path(audio_folder, audio_name))
+    cover_name = f'{uuid.uuid4()}.png'
+    cover_path = str(Path(cover_folder, cover_name))
+
+    try:
+        if update.message.audio:
+            await download_audio(update.message.audio, context, audio_path, cover_path, max_duration)
+
+        elif update.message.video and user.is_premium:
+            video = update.message.video
+            text = '''
+                🔃Відео завантажується...
+            '''
+            message = await context.bot.send_message(chat_id=chat_id, text=text)
+            await download_video(video, context, audio_folder, max_duration)
+            text = '''
+                📩Відео успішно завантажено!
+            '''
+            await message.edit_text(text=text)
+
+        elif update.message.text and user.is_premium:
+            text = '''
+                🔃Відео завантажується...
+            '''
+            message = await context.bot.send_message(chat_id=chat_id, text=text)
+            await download_audio_from_youtube(update.message.text, chat_id, context, audio_folder, cover_folder, max_duration)
+            text = '''
+                📩Ютуб-Відео успішно завантажено!
+            '''
+            await message.edit_text(text=text)
+        elif (update.message.video and not user.is_premium) or (update.message.text and not user.is_premium):
+            text = '''
+                Дана функція доступна лише преміум-користувачам.
+                \n/premium
+            '''
+            await context.bot.send_message(chat_id=chat_id, text=text)
+            return ConversationHandler.END
+    except AllowedVideoDurationExceededError:
+        time_left = datetime.timedelta(seconds=max_duration)
+        text = f'''
+            ❌Довживна занадто велика. Ваш аккаунт дозволяє завантажувати максимальною довжиною в {
+                format_timedelta(time_left, format="short", locale="uk_UA")
+            }
         '''
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+        await context.bot.send_message(chat_id=chat_id, text=text)
         return ConversationHandler.END
-    if result is not None and result == -1:
+    except BadRequest:
+        text = '''
+            ❌Розмір файлу занадто великий. Спробуйте зменшити його розмір, або ви можете придбати преміум та завантажити його на ютуб
+        '''
+        await context.bot.send_message(chat_id=chat_id, text=text)
+        return ConversationHandler.END
+    except subprocess.CalledProcessError:
+        text = '''
+            ❌Під час завантаження відео виникла помилка!
+        '''
+        await context.bot.send_message(chat_id=chat_id, text=text)
         return ConversationHandler.END
     
 
